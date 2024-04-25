94d3ec703567488156ff38abdadea5c4e4700329
Khen I. Abesamis <abesamiskheni@gmail.com>
2024-04-25 20:38:19 +0800

    first commit

---
+++ FrontPage/home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FRONTPAGE</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="background">
+                <div class="container nav-color">  
+                    <div class="ellipse">
+                        <img src="/img/1.png" alt="Company Logo">
+                    </div>
+                    <ul class="navBar">
+                        <li class="navigation">INVENTORY</li>
+                        <li class="navigation">STOCK REPORT</li>
+                        <li class="navigation">SALES REPORT</li>
+                        <li class="navigation">CONTACTS</li>
+                    </ul>
+                </div>
+            <div>
+            <div class="box">
+                <div class="bike-container">
+                <img class="bike" src="/img/3.png" alt="bike">
+                <img class="bike2" src="/img/3.png" alt="bike">
+                <img class="bike3" src="/img/3.png" alt="bike">
+                </div>
+                <img class="company-contacts" src="/img/2.jpg" alt="company-contacts">
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ tuts/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+    $name= 'Yoshi';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta width="device-width, initial-scale=1.0">
+    <title>User Profile Page</title>
+</head>
+<body>
+
+</body>
+</html>
